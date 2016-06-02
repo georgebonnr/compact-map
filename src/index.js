c9aee3f527c41868435786d5ece3fe829d3dc057
George Bonner <georgebonnr@gmail.com>
2016-06-02 15:41:10 -0700

    import null-like dependency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import isNullLike from 'null-like'
+
 export default (collection, callback) => collection.reduce((memo, value, index, collection) => {
     const result = callback(value, index, collection)
     if (!isNullLike(result)) memo.push(result)
